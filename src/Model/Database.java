6c3de1ce52c67c7f2845d39b3acace0b547a135d
MSaula <miquelsaula@gmail.com>
2018-04-13 12:00:04 +0200

    database clean

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -63,41 +63,35 @@ public class Database {
         while (rs.next()) lastID = rs.getLong("id");
     }
 
+    /**
+     * Mètode que retorna el ID d'una nova transacció. Incrementa automàticament el valor
+     * per automatitzar la obtenció de IDs.
+     *
+     * IMPORTANT: Aconseguir el ID nomès a partir d'aquesta funció, mai de l'atribut
+     * @return ID d'una nova transacció
+     */
     private static long getLastID() {
         lastID++;
         return lastID;
     }
 
-/*
-@deprecated
-    //Mètode que executa una Query a la base de dades per demanar informació.
-    private static ResultSet selectQuery(String query){
-        try {
-            Statement s = conn.createStatement();
-            return s.executeQuery(query);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    //Mètode que executa una Query a la base de dades per modificar informació.
-    private static void insertQuery(String query){
-        try {
-            Statement s = conn.createStatement();
-            s.executeUpdate(query);
-        } catch (Exception e) {
-            //TODO: gestionar degudament
-            e.printStackTrace();
-        }
-    }
-*/
-    public static void insertNewUser(User user) throws Exception { // TODO: fer transacció inicial de cash
+    /**
+     * Funció que afegeix un nou usuari a la base de dades a la taula d'Usuaris.
+     * A més a més també afegeix una transacció inicial amb la quantitat de diners inicial,
+     * i afegeix el nivell inicial de l'usuari.
+     * La funció no comprova si l'usuai ja existeix, per tant en cas d'estar repetit generarà
+     * una excepció al ser "username" una PK de la taula.
+     * @param user Usuari a registrar
+     * @throws Exception En cas d'estar repetit o bé de fallar una de les Querys.
+     */
+    public static void insertNewUser(User user) throws Exception {
+        //Inserim l'usuari a Users
         conn.createStatement().executeUpdate("insert into Users (username, mail, password) values ('" +
                 user.getUsername() + "', '" +
                 user.getMail() + "', '" +
                 user.getPassword() + "')");
 
+        //Realitzem la transacció de diners inicials
         conn.createStatement().executeUpdate("insert into Transactions (id, earnings, type, username) values (" +
                 getLastID() + ", '" +
                 Casino_Server.WELCOME_GIFT + "', " +
@@ -105,6 +99,7 @@ public class Database {
 
         user.setWallet(Casino_Server.WELCOME_GIFT);
 
+        //Iniciem l'usuari en el nivell inicial
         conn.createStatement().executeUpdate("insert into Levels (username, currentWallet) values ('" + user.getUsername() +
                 "', '" + user.getWallet() + "')");
     }
@@ -113,24 +108,28 @@ public class Database {
      * Mètode per a actualitzar la informació d'un usuari a la base de dades.
      * S'utilitza com a referencia el nom d'usuari, per tant tot el que s'hagi modificat
      * de l'usuari en sí es reescriurà a la base de dades.
+     *
+     * Important: És necessari que l'usuari a modificar tingui com a mínim inserida la
+     * informació del username i la password
+     *
      * @param online Indica si l'usuari esta o no en línea
      * @param user Usuari a actualitzar
      */
     public static void updateUser(User user, boolean online) throws Exception {
-
-        String lastLogin = online ? "" : "'null'";
-
-        conn.createStatement().executeUpdate("update Users set " +
-                        "lastLogin = " + lastLogin + ", " +
-                        CNAME_MAIL + "='" + user.getMail() + "', " +
+        if (online) conn.createStatement().executeUpdate("update Users set " +
+                        "lastLogin = NULL, " +
                         CNAME_PASSWORD + "='" + user.getPassword() + "'" +
                         "where " + CNAME_USERNAME + "='" + user.getUsername() + "'");
+        else conn.createStatement().executeUpdate("update Users set " +
+                "lastLogin = CURRENT_TIMESTAMP, " +
+                CNAME_PASSWORD + "='" + user.getPassword() + "'" +
+                "where " + CNAME_USERNAME + "='" + user.getUsername() + "'");
     }
 
     /**
      * Mètode per a eliminar un usuari de la base de dades
      * @param user Usuari a eliminar
-     * @deprecated No es pot eliminar a un usuari
+     * @deprecated No es pot eliminar a un usuari - test usage only
      */
     public static void deleteUser(User user) throws Exception {
         deleteUser(user.getUsername());
@@ -139,118 +138,10 @@ public class Database {
     /**
      * Mètode per a eliminar un usuari de la base de dades a partir del nom d'usuari
      * @param username Nom de l'usuari
-     * @deprecated No es pot eliminar a un usuari
+     * @deprecated No es pot eliminar a un usuari - test usage only
      */
     public static void deleteUser(String username) throws Exception {
         conn.createStatement().executeUpdate("delete from Users where username='" + username + "'");
-    }
-/*
-@deprecated
-    //Mètode que obté la String a escriure a la base de dades a partir de la llista oroginal
-    private static String coinHistoryToString(ArrayList<Long> coinHistory) {
-        String s = coinHistory.size() > 1 ? coinHistory.get(0).toString() : "";
-        for (int i = 1; i < coinHistory.size(); i++) s += "_" + coinHistory.get(i);
-        return s;
-    }
-*/
-    //Funció que indica si un nom correspon a una possible columna de la taula de la bdd
-    private static boolean comprovaColumnName(String name) { //Near to deprecation
-        boolean b = false;
-
-        for (String s: COLUMN_NAMES) if (name.equals(s)) b = true;
-        return b;
-    }
-
-    /**
-     * Funció que serveix per a aconseguir un conjunt d'informació de la base de dades.
-     * El seu funcionament consisteix en demanar tots els camps dels quals es requereixi la
-     * informació com a paràmetres, i finalment s'obtindrà com a resultat un llistat amb
-     * tota la informació organitzada.
-     * Cal dir que aquesta funció només permet obtenir informació emmagatzemada com a Text,
-     * és a dir que no es podràn obtenir les monedes (coin history sí).
-     * @param columnNames Llistat de noms de les columnes a consultar
-     * @return Llista d'arrays de Strings amb tots els camps demanats
-     * @throws Exception En cas de demanar alguna informació inexistent.
-     * @deprecated
-     */
-    public static LinkedList<String[]> getInfo(String ... columnNames) throws Exception {
-        //Es fa la petició al servidor de la database
-        ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM Users");
-
-        //Es comprova que totes les columnes demanades siguin existents
-        for (String s: columnNames) if (s.equals("wallet") || !comprovaColumnName(s))
-            throw new Exception("La columna que es vol seleccionar no existeix");
-
-        //Es recull tota la informació
-        try {
-            LinkedList<String[]> info = new LinkedList<>();
-
-            while (rs.next()) {
-                String[] aux = new String[columnNames.length];
-                for (int i = 0; i < columnNames.length; i++) aux[i] = rs.getString(columnNames[i]);
-                info.add(aux);
-            }
-
-            return info;
-        } catch (Exception e) {
-            //TODO: gestiona esto tete
-            e.printStackTrace();
-
-            return new LinkedList<>();
-        }
-    }
-
-    /**
-     * @deprecated
-     */
-    public static LinkedList<String> getUserInfo(String username) throws SQLException{
-        ResultSet rs = conn.createStatement().executeQuery(
-                "select * from Users where username='" + username + "'");
-
-        LinkedList<String> info = new LinkedList<>();
-        rs.next();
-        info.add(rs.getString(CNAME_PASSWORD));
-        info.add(rs.getString(CNAME_MAIL));
-        info.add(rs.getString(CNAME_WALLET));
-        info.add(rs.getString(CNAME_COINHISTORY));
-
-        return info;
-    }
-
-    /**
-     * Funció que retorna un array amb tota la evolució de la cartera del usuari
-     * indicat.
-     * @param username Nom del usuari del que es vol cercar la informació
-     * @return Array amb tots els valors registrats de l'usuari indicat
-     * @throws Exception En cas de no haver trobat l'usuari indicat
-     * @deprecated
-     */
-    public static ArrayList<Long> getUserCoinHistory(String username) throws Exception {
-        LinkedList<String[]> info = getInfo(CNAME_USERNAME, CNAME_COINHISTORY);
-        String coinHistory = null;
-
-        for (String[] s: info) if (s[0].equals(username)) coinHistory = s[1];
-        if (coinHistory == null) throw new Exception("No s'ha trobat l'usuari");
-
-        String[] coins = coinHistory.split("_");
-        ArrayList<Long> parsedHistory = new ArrayList<>();
-
-        for (int i = 0; i < coins.length; i++) parsedHistory.add(Long.parseLong(coins[i]));
-
-        return parsedHistory;
-    }
-
-    /**@deprecated */
-    public static void test() {
-        try {
-            ResultSet rs = conn.createStatement().executeQuery("select password from Users where username = 'miquelsaula'");
-            while(rs.next()) {
-                System.out.println(rs.getString("password") + " - " + rs.getString("username"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
 
     /**
@@ -265,24 +156,11 @@ public class Database {
             String pass = rs.getString("password");
 
             user.setCredentialsOk(user.getPassword().equals(pass));
-            //user.setOnline(user.getPassword().equals(pass));
-
             return user;
         } catch (Exception e) {
             user.setCredentialsOk(false);
-            //user.setOnline(false);
             return user;
         }
-    }
-
-    //Funció que converteix un coinHistory de format String a format usable
-    private static ArrayList<Long> parseCoinHistory(String coinHistoryString) {
-        String[] coins = coinHistoryString.split("_");
-        ArrayList<Long> parsedHistory = new ArrayList<>();
-
-        for (int i = 0; i < coins.length; i++) parsedHistory.add(Long.parseLong(coins[i]));
-
-        return parsedHistory;
     }
 
     /**
@@ -291,36 +169,48 @@ public class Database {
      * @throws Exception En cas de no coincidir la contrasenya
      */
     public static void fillUser(User user) throws Exception {
-        LinkedList<String[]> info = getInfo("username", "password", "mail");
+        ResultSet rsm = conn.createStatement().executeQuery("select mail, password from Users where username = '" + user.getUsername() + "';");
+        rsm.next();
 
-        for (String[] s: info) if (s[0].equals(user.getUsername())) {
-            if (!user.getPassword().equals(s[1])) throw new Exception("No coincideix la contrassenya");
-            user.setMail(s[2]);
-            user.setWallet(getUserWallet(user.getUsername()));
-        }
+        if (!rsm.getString("password").equals(user.getPassword())) throw new Exception("Incorrect password");
+
+        user.setMail(rsm.getString("mail"));
+        user.setWallet(getUserWallet(user.getUsername()));
     }
 
-    //Reconstrueix el wallet d'un usuari a partir de l'historial de transaccions
+    /**
+     * Reconstrueix el wallet d'un usuari a partir de l'historial de transaccions
+     * @param username nom del usuari del que es desitja obtenir els diners
+     * @return diners actuals de l'usuari
+     * @throws Exception En cas de no existir l'usuari o fallar la connexió
+     */
     public static long getUserWallet(String username) throws Exception {
         ResultSet rs = conn.createStatement().executeQuery("select earnings from Transactions where username = '" + username + "';");
         long wallet = 0;
 
         while (rs.next()) wallet += rs.getLong("earnings");
-
         return wallet;
     }
 
+    /**
+     * Mètode per a comprovar si un nom ja està usat per un usuari
+     * @param username Nom a comprovar
+     * @return Si ja està agafat el nom
+     */
     public static boolean usernamePicked(String username) {
         try {
             ResultSet rs = conn.createStatement().executeQuery("select username from Users");
             while (rs.next()) if (username.equals(rs.getString("username"))) return true;
         } catch (SQLException e) {
-            //TODO
             e.printStackTrace();
         }
         return false;
     }
 
+    /**
+     * Mètode que registra una nova transacció a la base de dades.
+     * @param transaction Transacció a registrar
+     */
     public static void registerTransaction(Transaction transaction) {
         try {
             conn.createStatement().executeUpdate("insert into Transactions (username, id, earnings, type) values ('" + transaction.getUsername()
@@ -332,7 +222,13 @@ public class Database {
         }
     }
 
-
+    /**
+     * Mètode que donada una referencia de temps en format Timestamp,
+     * obté una representació "user fiendly" del temps que ha passat
+     * des del moment indicat fins al moment actual.
+     * @param back Referencia de temps
+     * @return Representació del temps passat
+     */
     public static String getPassedTime(Timestamp back) {
         Timestamp now = Timestamp.from(Instant.now());
 
@@ -342,11 +238,15 @@ public class Database {
         else if (diff < 60 * 60) return diff/60 + "min ago";
         else if (diff < 60 * 60 * 24) return diff/(60*60) + "h ago";
         else if (diff < 60 * 60 * 24 * 2) return diff/(60*60*24) + "day ago";
-        else return diff/(60*60*24) + "days ago";
+        else if (diff < 60 * 60 * 24 * 7) return diff/(60*60*24) + "days ago";
+        else if (diff < 60 * 60 * 24 * 30) return diff/(60*60*24*7) + "weeks ago";
+        else if (diff < 60 * 60 * 24 * 365) return diff/(60*60*24*30) + "moths ago";
+        else return diff/(60*60*24*365) + "years ago";
     }
 
     /**
-     * Busca en la DB el nivell del usuari. D'aquesta manera l'usuari te un color del dorsal de les seves cartes segons el nivell amb el que es troba
+     * Busca en la DB el nivell del usuari. D'aquesta manera l'usuari te un color del
+     * dorsal de les seves cartes segons el nivell amb el que es troba
      * @param username Usuari que vol obtenir una carta
      * @return El color del dorsal que ha d'obtenir, basat en el seu nivell actual.
      */
@@ -383,6 +283,11 @@ public class Database {
         return "back-red.png";
     }
 
+    /**
+     * Funció que retorna una taula amb tota la informació necessària a mostrar
+     * a la taula de ranking d'usuaris
+     * @return Matriu dobjectes amb la informació a mostrar
+     */
     public static Object[][] getInfoRank(){
         try {
             ResultSet rs = conn.createStatement().executeQuery("select * from Users;");
@@ -399,7 +304,7 @@ public class Database {
                     s[1] = 0;
                 }
 
-                s[2] = rs.getTimestamp("lastLogin");
+                s[2] = getPassedTime(rs.getTimestamp("lastLogin"));
 
                 users.add(s);
             }
